4a2129b278f9325e7df844aca061c526c03798bf
Slavcho Ivanov <slavcho@gmail.com>
2020-07-24 18:02:44 +0300

    add the machine id as a fallback to the macaddress.

--- src/macrometa.c
+++ src/macrometa.c
@@ -4,6 +4,11 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#include <sys/ioctl.h>
+#include <net/if.h>
+#include <unistd.h>
+#include <netinet/in.h>
+
 #define BLOCKED_API_URL "/_api/ipbl/blocked/"
 #define REGISTER_ID_API_URL "/_api/ipbl/register"
 
@@ -39,13 +44,55 @@ int is_blacklisted_mm(const char* ip, const char* app_key, const char* endpoint,
     return blacklisted;
 }
 
-int register_machine(const char* app_key, const char* endpoint, char ipblc_id[MAX_BODY])
+int read_mac_address(char *mac_address)
 {
-    char url[MAX_URL] = "";
-    strcat(url, endpoint);
-    strcat(url, REGISTER_ID_API_URL);
+    struct ifreq ifr;
+    struct ifconf ifc;
+    char buf[1024];
+    int success = 0;
 
-    char machine_id[255] = {'\0'};
+    int sock = socket(AF_INET, SOCK_DGRAM, IPPROTO_IP);
+    if (sock == -1) { /* handle error*/ };
+
+    ifc.ifc_len = sizeof(buf);
+    ifc.ifc_buf = buf;
+    if (ioctl(sock, SIOCGIFCONF, &ifc) == -1) {
+        return -1;
+    }
+
+    struct ifreq* it = ifc.ifc_req;
+    const struct ifreq* const end = it + (ifc.ifc_len / sizeof(struct ifreq));
+
+    for (; it != end; ++it) {
+        strcpy(ifr.ifr_name, it->ifr_name);
+        if (ioctl(sock, SIOCGIFFLAGS, &ifr) == 0) {
+            if (! (ifr.ifr_flags & IFF_LOOPBACK)) { // don't count loopback
+                if (ioctl(sock, SIOCGIFHWADDR, &ifr) == 0) {
+                    success = 1;
+                    break;
+                }
+            }
+        }
+        else { 
+            /* handle error */ 
+            printf("failed to read address of %s\n", it->ifr_name);
+        }
+    }
+
+    unsigned char mac[6];
+
+    if (success) {
+        memcpy(mac, ifr.ifr_hwaddr.sa_data, 6);
+    } else {
+        return -1;
+    }
+
+    sprintf(mac_address, "%.2X:%.2X:%.2X:%.2X:%.2X:%.2X" , mac[0], mac[1], mac[2], mac[3], mac[4], mac[5]);
+    return 0;
+}
+
+int read_machine_id(char *machine_id) 
+{
     FILE *fp = fopen("/etc/machine-id", "r");
     if (fp == NULL)
     {
@@ -53,6 +100,27 @@ int register_machine(const char* app_key, const char* endpoint, char ipblc_id[MA
     }
     char* res = fgets(machine_id, 255, (FILE*)fp);
     fclose(fp);
+
+    return 0;
+}
+
+int register_machine(const char* app_key, const char* endpoint, char ipblc_id[MAX_BODY])
+{
+    char url[MAX_URL] = "";
+    strcat(url, endpoint);
+    strcat(url, REGISTER_ID_API_URL);
+
+    char machine_id[255] = {'\0'};
+    if (read_mac_address(machine_id) != 0)
+    {
+        printf("Failed to read mac address. Falling back to machine id.\n");
+        if (read_machine_id(machine_id) != 0) {
+            printf("Failed to read machine id as well.\n");
+            return -4;
+        }
+    }
+
+    printf("machine id : %s\n", machine_id);
 
     struct curl_request req = create_curl_request(url);
     strncpy(req.authorization, app_key, MAX_BODY);
